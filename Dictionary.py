b6ddf8dfd9a6cdab88a64b374933a969a535c7a3
wine-planetary <tajiri@wine-planetary.com>
2020-12-28 11:32:32 +0900

    bugfix

--- Dictionary.py
+++ Dictionary.py
@@ -6,7 +6,7 @@ dicpath = os.path.join(rootdir, "ipcdic_wine.dic")
 
 if not os.path.exists(dicpath):
     print("creating dictionary...")
-    cmd = "mecab-dict-index -f utf-8 -t utf-8 -d "C:\Program Files\MeCab\dic\ipadic" -u ipcdic_wine.dic wine.csv"
+    cmd = "mecab-dict-index -f utf-8 -t utf-8 -d \"C:\Program Files\MeCab\dic\ipadic\" -u ipcdic_wine.dic wine.csv"
     subprocess.run(cmd)
 
 def get_dic_path():
